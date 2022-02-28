f958590304f73bed22f80d139123952c15bff9c9
Bjarnelp <bjarne.l.pedersen@gmail.com>
2022-02-28 06:15:40 +0100

    Uploaded/updated win/RStudio 28-02-22

--- Testing.R
+++ Testing.R
@@ -1,27 +1,29 @@
-require(tidyverse)
 require(knitr)
 require(REdaS)
 require(glue)
-require(tidymodels)
 require(glmnet)
 require(LiblineaR)
 require(kernlab)
 require(kknn)
 require(bestNormalize)
-require(finetune)
 require(moments)
 require(ggcorrplot)
 require(corrr)
-require(workflowsets)
 require(treesnip)
 require(ranger)
-require(baguette)
 require(xgboost)
 require(lightgbm)
 require(lme4)
+
+require(tidyverse)
+require(tidymodels)
+require(finetune)
+require(workflowsets)
+require(baguette)
 require(parallel)
 require(doParallel)
 require(stringr)
+
 
 all_cores <- parallel::detectCores(logical = FALSE)
 
@@ -108,7 +110,8 @@ train <- left_join(train,Impute_table,by=c("Title")) %>%
   mutate(Age = if_else(is.na(Age),mean_age,Age)) %>%
   mutate(Fare = if_else(is.na(Fare),mean_fare,Fare)) %>% 
   mutate(mean_age = NULL) %>% 
-  mutate(mean_fare = NULL)
+  mutate(mean_fare = NULL) %>% 
+  mutate(Embarked = if_else(is.na(Embarked),"U",Embarked))
 
 test <- left_join(test,Impute_table,by=c("Title")) %>% 
   mutate(Age = if_else(is.na(Age),mean_age,Age)) %>%
@@ -116,11 +119,6 @@ test <- left_join(test,Impute_table,by=c("Title")) %>%
   mutate(mean_age = NULL) %>% 
   mutate(mean_fare = NULL)
   
-  
-  left_join(test,Agetable,by=c("Title")) %>% 
-  mutate(Age = if_else(is.na(Age),mean,Age)) %>% 
-  mutate(mean = NULL) 
-
 train %>% 
   select(which(colSums(is.na(.))>0)) %>% 
   summarise_all(~ sum(is.na(.)))
@@ -131,25 +129,87 @@ test %>%
 
 rec <- 
   recipe(Survived ~ ., data=train) %>% 
-  update_role(PassengerId,new_role="ID") %>% 
+  update_role(PassengerId,new_role="ID")
+
+%>% 
   step_impute_mode(Embarked)
 
 folds <- vfold_cv(train,strata = Survived)
-
-model <-
-  decision_tree(min_n=tune()) %>% 
-  set_engine("C5.0") %>% 
-  set_mode("classification")
 
 bagged_model <-
   bag_tree(min_n=tune()) %>% 
   set_engine("C5.0") %>% 
   set_mode("classification")
 
+model <-
+  decision_tree(min_n=tune()) %>% 
+  set_engine("C5.0") %>% 
+  set_mode("classification")
+
+bm4 <-
+  bag_tree(min_n=4) %>% 
+  set_engine("C5.0") %>% 
+  set_mode("classification")
+
+bm6 <-
+  bag_tree(min_n=6) %>% 
+  set_engine("C5.0") %>% 
+  set_mode("classification")
+
+bm7 <-
+  bag_tree(min_n=7) %>% 
+  set_engine("C5.0") %>% 
+  set_mode("classification")
+
+bm8 <-
+  bag_tree(min_n=8) %>% 
+  set_engine("C5.0") %>% 
+  set_mode("classification")
+
+bm9 <-
+  bag_tree(min_n=9) %>% 
+  set_engine("C5.0") %>% 
+  set_mode("classification")
+
+bm10 <-
+  bag_tree(min_n=10) %>% 
+  set_engine("C5.0") %>% 
+  set_mode("classification")
+
 workflowset <- 
   workflow_set(
     preproc = list(age = rec),
-    models = list(c50=model,bag=bagged_model))
+    models = list(c50=model))
+
+wfs_4 <- 
+  workflow_set(
+    preproc = list(rec = rec),
+    models = list(n4 = bm4))
+
+wfs_6 <- 
+  workflow_set(
+    preproc = list(rec = rec),
+    models = list(n6 = bm6))
+
+wfs_7 <- 
+  workflow_set(
+    preproc = list(rec = rec),
+    models = list(n7 = bm7))
+
+wfs_8 <- 
+  workflow_set(
+    preproc = list(rec = rec),
+    models = list(n8 = bm8))
+
+wfs_9 <- 
+  workflow_set(
+    preproc = list(rec = rec),
+    models = list(n9 = bm9))
+
+wfs_10 <- 
+  workflow_set(
+    preproc = list(rec = rec),
+    models = list(n10 = bm10))
 
 c50_control <- control_grid(
   save_pred = TRUE,
@@ -157,42 +217,152 @@ c50_control <- control_grid(
   save_workflow = TRUE
 )
 
-c50_results <- 
+c50_res <- 
   workflowset %>% 
   workflow_map(
     seed = 1234,
     resamples = folds,
-    grid = 10,
-    control = _control, 
+    control = c50_control, 
     metrics = metric_set(accuracy,roc_auc),
     verbose = TRUE
   )
 
-autoplot(c50_results,rank_metric="accuracy") +
+bmc <- control_grid(
+  save_pred = TRUE,
+  parallel_over = "everything",
+  save_workflow = TRUE
+)
+
+res_4 <- 
+  wfs_4 %>% 
+  workflow_map(
+    seed = 1234,
+    resamples = folds,
+    control = bmc, 
+    metrics = metric_set(accuracy,roc_auc),
+    verbose = TRUE
+  )
+
+res_6 <- 
+  wfs_6 %>% 
+  workflow_map(
+    seed = 1234,
+    resamples = folds,
+    control = bmc, 
+    metrics = metric_set(accuracy,roc_auc),
+    verbose = TRUE
+  )
+
+res_7 <- 
+  wfs_7 %>% 
+  workflow_map(
+    seed = 1234,
+    resamples = folds,
+    control = bmc, 
+    metrics = metric_set(accuracy,roc_auc),
+    verbose = TRUE
+  )
+
+res_8 <- 
+  wfs_8 %>% 
+  workflow_map(
+    seed = 1234,
+    resamples = folds,
+    control = bmc, 
+    metrics = metric_set(accuracy,roc_auc),
+    verbose = TRUE
+  )
+
+res_9 <- 
+  wfs_9 %>% 
+  workflow_map(
+    seed = 1234,
+    resamples = folds,
+    control = bmc, 
+    metrics = metric_set(accuracy,roc_auc),
+    verbose = TRUE
+  )
+
+res_10 <- 
+  wfs_10 %>% 
+  workflow_map(
+    seed = 1234,
+    resamples = folds,
+    control = bmc, 
+    metrics = metric_set(accuracy,roc_auc),
+    verbose = TRUE
+  )
+
+bm_res <- bind_rows(
+  res_4,
+  res_6,
+  res_7,
+)
+
+autoplot(bm_res,rank_metric="accuracy") +
   geom_text(aes(y=0.55,label= wflow_id), angle=90) +
   lims(y=c(0.3,0.9)) +
   theme(legend.position = "none")
 
-c50_best_parm <-
-  c50_results %>% 
-  extract_workflow_set_result("age_bag") %>% 
+bm4_parm <-
+  bm_res %>% 
+  extract_workflow_set_result("rec_n4") %>% 
   select_best(metric="accuracy")
-c50_best_parm
 
-c50_wf <- c50_results %>% 
-  extract_workflow("age_bag") %>% 
-  finalize_workflow(c50_best_parm)
+bm6_parm <-
+  bm_res %>% 
+  extract_workflow_set_result("rec_n6") %>% 
+  select_best(metric="accuracy")
 
-c50_mod_age <- fit(c50_wf,train)
+bm7_parm <-
+  bm_res %>% 
+  extract_workflow_set_result("rec_n7") %>% 
+  select_best(metric="accuracy")
 
-c50_pred_age <- predict(c50_mod_age,new_data=test)
+bm10_parm <-
+  bm_res %>% 
+  extract_workflow_set_result("rec_n10") %>% 
+  select_best(metric="accuracy")
 
-cbind(PassengerId = test$PassengerId,c50_pred_age) %>% 
+bm4_wf <- bm_res %>% 
+  extract_workflow("rec_n4") %>% 
+  finalize_workflow(bm4_parm)
+
+bm6_wf <- bm_res %>% 
+  extract_workflow("rec_n6") %>% 
+  finalize_workflow(bm6_parm)
+
+bm7_wf <- bm_res %>% 
+  extract_workflow("rec_n7") %>% 
+  finalize_workflow(bm7_parm)
+
+bm10_wf <- bm_res %>% 
+  extract_workflow("rec_n10") %>% 
+  finalize_workflow(bm10_parm)
+
+bm4_fit <- fit(bm4_wf,train)
+bm6_fit <- fit(bm6_wf,train)
+bm7_fit <- fit(bm7_wf,train)
+bm10_fit <- fit(bm10_wf,train)
+
+bm4_pred <- predict(bm4_fit,new_data=test)
+bm6_pred <- predict(bm6_fit,new_data=test)
+bm7_pred <- predict(bm7_fit,new_data=test)
+bm10_pred <- predict(bm10_fit,new_data=test)
+
+cbind(PassengerId = test$PassengerId,bm4_pred) %>% 
   rename(Survived = .pred_class) %>% 
-  write_csv("./c50_pred_age.csv")
+  write_csv("./bm4_pred.csv")
+cbind(PassengerId = test$PassengerId,bm6_pred) %>% 
+  rename(Survived = .pred_class) %>% 
+  write_csv("./bm6_pred.csv")
+cbind(PassengerId = test$PassengerId,bm7_pred) %>% 
+  rename(Survived = .pred_class) %>% 
+  write_csv("./bm7_pred.csv")
 
 # Med FamilyName og Title giver bagged c5.0 en score på 0.80143 - placering 369.
-
+# Impute_mode er bedre end at sætte NA="U"
+# Bedste er n=6 eller 7 - hvordan genskabes over 80%?
 # RF
 
 rf_model <- rand_forest(trees = tune(), min_n=tune()) %>% 
@@ -288,3 +458,51 @@ imp_results <-
     metrics = metric_set(accuracy,roc_auc),
     verbose = TRUE
   )
+
+Since the random forest classifier will have a problem with dealing with the family name column, we will do the dummy encoding separately before fitting the random forest:
+  ```{r}
+imp_rec_rf <- imp_rec %>% 
+  step_dummy(FamilyName)
+```
+
+imp_svm_lin <- svm_linear(cost=tune()) %>% 
+  set_mode("classification") %>% 
+  set_engine("kernlab", scaled=TRUE)
+imp_svm_pol <- svm_poly(cost=tune(),degree=tune()) %>% 
+  set_mode("classification") %>% 
+  set_engine("kernlab", scaled=TRUE)
+imp_svm_radial <- svm_rbf(cost=tune(),rbf_sigma=tune()) %>% 
+  set_mode("classification") %>% 
+  set_engine("kernlab", scaled=TRUE)
+
+imp_rf <- 
+  rand_forest(trees = tune(), min_n=tune()) %>% 
+  set_engine("randomForest") %>% 
+  set_mode("classification")
+
+
+imp_workflowset2 <- 
+  workflow_set(
+    preproc = list(rec = imp_rec),
+    models = list(svm_poly = imp_svm_pol,
+                  svm_rbf = imp_svm_radial))
+
+imp_workflowset3 <-
+  workflow_set(
+    preproc = list(rec = imp_rec_rf),
+    models = list(rf = imp_rf))
+
+imp_workflowset4 <- bind_rows(imp_workflowset2,imp_workflowset3)
+
+imp_results2 <- 
+  imp_workflowset3 %>% 
+  workflow_map(
+    seed = 1234,
+    resamples = imp_folds,
+    grid = 25,
+    control = imp_control, 
+    metrics = metric_set(accuracy,roc_auc),
+    verbose = TRUE
+  )
+
+imp_results <- bind_rows(imp_results1,imp_results2)
--- Titanic/Titanic.R
+++ Titanic/Titanic.R
@@ -18,10 +18,27 @@ titles <- c("Mr.","Mrs.","Miss.","Ms.","Master.","Dr.","Rev.","Mlle.","Mme.","Si
 sexes <- c("Male","Female")
 embarks <- c("Cherbourg","Queenstown","Southampton")
 
+train_data <- read_csv("./tt_train_pp.csv",show_col_types = FALSE)
+
+train_data <- train_data %>% 
+  mutate(Survived = as_factor(Survived)) %>% 
+  mutate(Pclass = as_factor(Pclass))
+
+rec <- recipe(Survived ~.,data=train_data) %>% 
+  update_role(PassengerId,new_role = "ID") %>% 
+  step_impute_mode(Embarked)
+
+model <- 
+  bag_tree(min_n=7) %>% 
+  set_engine("C5.0") %>% 
+  set_mode("classification")
+
+wf <- workflow(rec,model)
+
+model_fit <- fit(wf,train_data)
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    useShinyFeedback(),
-
     # Application title
     titlePanel("Titanic Survival Predictor"),
 
@@ -30,14 +47,14 @@ ui <- fluidPage(
         sidebarPanel(
             selectInput("Title","Title",titles),         #Title input
             textInput("First","First Name(s)",value=""),                                  #First name input
-            textInput("Last Name","FamilyName",value=""),                                  #Last name input
-            radioButtons("PClass","Passenger class",classes,inline=T),                                  #Passenger Class
+            textInput("FamilyName","Last Name",value=""),                                  #Last name input
+            radioButtons("Pclass","Passenger class",choiceNames=c("1st","2nd","3rd"),choiceValues=classes,inline=T),                                  #Passenger Class
             radioButtons("Sex","Sex",sexes,inline=T),                                  #Sex
-            numericInput("Age","Age",value=NA,min=0,max=200),
+            numericInput("Age","Age",value=20,min=0,max=200),
             numericInput("SibSp","Number of siblings and/or Spouses travelling with you",value=0,min=0,max=10),
             numericInput("Parch","Number of parents or children travelling with you",value=0,min=0,max=5),
             sliderInput("Fare","Fare paid",value=20,min=0,max=50,step=0.2,ticks=FALSE),
-            radioButtons()
+            radioButtons("Embark","Point of embarkation",embarks,inline=T)
         ),
 
         # Show a plot of the generated distribution
@@ -47,44 +64,25 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output,session) {
-      train_data <- read_csv("./tt_train_pp.csv")
 
-      train_data <- train_data %>% 
-        mutate(Survived = as_factor(Survived)) %>% 
-        mutate(Pclass = as_factor(Pclass))
+      title_input <- reactive(input$Title)
+      firstname_input <- reactive(input$First)
+      familyname_input <- reactive(input$FamilyName)
+      pclass_input <- reactive(input$Pclass)
+      sex_input <- reactive(input$Sex)
+      age_input <- reactive(input$Age)
+      sipsp_input <- reactive(input$SibSp)
+      parch_input <- reactive(input$Parch)
+      fare_input <- reactive(input$Fare)
       
-      rec <- recipe(Survived ~.,data=train_data) %>% 
-        update_role(PassengerId,new_role = "ID") %>% 
-        step_impute_mode(Embarked)
-      
-      model <- 
-        bag_tree(min_n=7) %>% 
-        set_engine("C5.0") %>% 
-        set_mode("classification")
-      
-      wf <- workflow(rec,model)
-      
-      model_fit <- fit(wf,train_data)
-      
-      title_input <- reactive({input$Title})
-      firstname_input <- reactive({input$First})
-      familyname_input <- reactive({input$FamilyName})
-      pclass_input <- reactive({input$PClass})
-      sex_input <- reactive({input$Sex})
-      age_input <- reactive({input$Age})
-      sipsp_input <- reactive({input$SibSp})
-      parch_input <- reactive({input$Parch})
-      fare_input <- reactive({input$fare})
-      
-      pred_table <- tibble(PassengerId=c(2000),
-                           Pclass = pclass_input,
-                           Sex = sex_input,
-                           Age = age_input,
-                           SibSp = sibsp_input,
-                           Parch = parch_input,
-                           Fare = 
+      pred_table <- tibble(PassengerId=c(age_input,sipsp_input),
+#                           Pclass = c(pclass_input),
+#                           Sex = c(sex_input),
+#                           Age = c(age_input),
+#                           SibSp = c(sibsp_input),
+#                           Parch = c(parch_input),
+#                           Fare = c(fare_input)
                            )
       
       output$table <- renderDataTable(pred_table)
